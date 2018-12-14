844bbad7db84e81b5055237ef4bb399579a76ae2
Ratatinator97 <asidiras.csi@gmail.com>
2018-12-14 18:28:32 +0100

    aze

--- ClientRush2.go
+++ ClientRush2.go
@@ -17,9 +17,12 @@ func main() {
 		for EntreeServeur(message) != true {
 			//delay
 		}
+		fmt.Print("Recu msg serveur")
 		fmt.Fprintf(connexion, "TCCHAT_REGISTER"+"\t"+EntreeMsg(1)+"\n")
-		connecte := true //Il faudra handle les erreurs ici
+		fmt.Print(connexion, "TCCHAT_REGISTER"+"notre pseudo")
+		connecte = true //Il faudra handle les erreurs ici
 	}
+	fmt.Print("Etape connexion terminee")
 	for {
 		//Lecture msg serveur
 		MsgType := LectureMsgServeur(message)
@@ -45,6 +48,7 @@ func traitermsg(msg string) []string {
 func EntreeServeur(S string) bool {
 	TabS := strings.Split(S, "\t")
 	if TabS[0] == "TCCHAT_WELCOME" {
+		fmt.Print("On a recu le welcome")
 		return true
 	} else {
 		return false
@@ -57,12 +61,16 @@ func LectureMsg(Str string) []string {
 func LectureMsgServeur(Msg string) int {
 	TypeMsg := LectureMsg(Msg)
 	if TypeMsg[0] == "TCCHAT_BCAST" {
+		fmt.Print("msg_broadcast")
 		return 0
 	} else if TypeMsg[0] == "TCCHAT_USEROUT" {
+		fmt.Print("msg_Userout")
 		return 1
 	} else if TypeMsg[0] == "TCCHAT_USERIN" {
+		fmt.Print("msg UserIn")
 		return 2
 	} else {
+		fmt.Print("Bizzare")
 		return 666
 	}
 }
@@ -78,12 +86,15 @@ func EntreeMsg(MsgType int) string {
 				valide = true
 			}
 		}
+		fmt.Print("Votre nom est " + texte)
 		NvTexte := strings.TrimSuffix(texte, "\n")
+		fmt.Print("C est mieux comme ca " + NvTexte)
 		return NvTexte
 	} else if MsgType == 2 {
 		texte, _ := reader.ReadString('\n')
 		if texte != "\n" {
 			texte := strings.TrimSuffix(texte, "\n")
+			fmt.Print("Envoi de message" + texte)
 			return texte
 		}
 	}
