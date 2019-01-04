c08a63f61d7d7b221cb248132e08599fbc644dd5
flastare22 <45687091+flastare22@users.noreply.github.com>
2019-01-04 08:28:17 +0100

    Add files via upload

--- Serveur/main/main.go
+++ Serveur/main/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net"
 	"os"
+	"strconv"
 	"strings"
-	"time"
 )
 
 type Client struct {
+	actif bool
 	conn net.Conn
 }
 
@@ -18,121 +20,164 @@ func main(){
 
 	fmt.Println("Lancement du serveur...")
 
-	ln, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		fmt.Println("Read error:", err)
-		os.Exit(1)
-	}
 
-	buffer:=make(chan string,20)
-
-	var tabClient [10]Client
-
-	var tabConn [10]bool
-
-	for {
-		conn, err := ln.Accept()
+		ln, err := net.Listen("tcp",":8081")
 		if err != nil {
 			fmt.Println("Read error:", err)
+			os.Exit(1)
 		}
 
-		index := -1
+		buffer:=make(chan string,20)
+
+		var tabClient [10]Client
+
+		var tabConn [10]bool
 
 		for i:=0;i<len(tabConn);i++{
-			if tabConn[i] {
-				index=i
+			tabConn[i]=true
+		}
+
+		var pc [10]*Client
+
+		for i:=0 ; i<len(tabClient);i++{
+			pc[i]=&tabClient[i]
+		}
+
+		go answer(buffer,pc)
+
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				fmt.Println("Read error:", err)
+				os.Exit(1)
+			} else {
+				fmt.Println("Connection detected")
+			}
+
+			index := -1
+
+			for i := 0; i < len(tabConn); i++ {
+				if tabConn[i]==true {
+					index = i
+				}
+			}
+
+			if (index != -1) {
+				fmt.Println("Connection available. Index : "+strconv.Itoa(index))
+				tabConn[index] = false
+				pC := &tabConn[index]
+				tabClient[index] = Client{true,conn}
+				go handleConnection(conn, buffer, pC, index)
+
+			} else {
+				fmt.Println("No connection available...")
+				go overSize(conn)
 			}
 		}
-
-		if(index!=-1){
-			go handleConnection(conn, buffer, tabConn, index)
-			tabClient[index]= Client{conn}
-			tabConn[index]=false
-		}else{
-			go overSize(conn)
-		}
-
-
-
-	}
 }
 
 func overSize(conn net.Conn){
 
 	conn.Write([]byte("Le navire est plein a craquer!!! Nous ne pouvons pas vous accepter!"))
-
 	conn.Close()
 }
 
-func answer(buffer chan string,) {
+func answer(buffer chan string, pointer [10]*Client ) {
 
 	for {
 
 		select {
 			case answer, ok := <-buffer:
 			if ok {
-				fmt.Printf("Value %d was read.\n", answer)
 
-				for i:=0;i<len(tabClient);i++{
+				formatAnsewer := join("> " ,answer,"\n")
 
-					tabClient[i].conn.Write([]byte(answer))
+				for i:=0;i<len(pointer);i++{
+
+					if pointer[i]!=nil {
+
+						pC :=pointer[i]
+
+						client:=*pC
+
+
+						if client.actif == true {
+							if _, err := client.conn.Write([]byte(formatAnsewer)); err != nil {
+								fmt.Println("Read error:", err)
+							}else{
+								fmt.Println("Message envoyé")
+							}
+						}
+					}
 				}
 
 			} else {
 				fmt.Println("Channel closed!")
 			}
 		default:
-			fmt.Println("No value ready")
+
 		}
 
 	}
 }
 
-func handleConnection (conn net.Conn, buffer chan string, tabConn [10]bool, index int){
+func handleConnection (conn net.Conn, buffer chan string, pC *bool , index int){
 
-	conn.Write([]byte("Bienvenu sur le serveur Chalutier Moussaillon! Quel est ton nom matelot?")) // send to the server/client
-
-	blackListed := false
+	conn.Write([]byte("TCCHAT_WELCOME\tLE_CHALUTIER_DE_L'ENFER\n"))
 
 	whiteList := readFile("whiteList.txt")
 
 	blackList := readFile("blackList.txt")
 
+	blackListed := false
+
 	name:=""
 
+	myReader:=bufio.NewReader(conn)
 
 	for{
 
-		buf := make([]byte, 0, 4096) // big buffer
-
-		_, err := conn.Read(buf)
+		message,err := myReader.ReadString('\n')
 
 		if err != nil {
 			if err != io.EOF {
 				fmt.Println("Read error:", err)
+				os.Exit(1)
 			}
-			break
 		}
 
-		name =string(buf)
 
-		fmt.Println("Ah "+name)
+		data:=strings.Split(message,"\t")
+
+		name=strings.TrimSuffix(data[1],"\n")
+
+		compteur:=strings.Count(name, "\n")
+
+		reponse:="Le nom du Client est: "+name
+
+		fmt.Println(reponse)
+
+		fmt.Println("Le nombre de putain d'espace est "+strconv.Itoa(compteur))
 
 		if(name!=""){
 
 			if(identification(name, blackList)){
-				conn.Write([]byte("Un pirate nous attaque!!!"))
+				conn.Write([]byte("TCCHAT_USERIN\tMais oui c'est ca! Tu es Barbe Rousse le Maquereau! Au secours! A l'aide\n"))
 				blackListed = true
 				break
 			}else{
 				if(identification(name, whiteList)){
-					conn.Write([]byte("Et bien, tu en as mis du temps pour decuver, vieux loup de mer!!!"))
-					buffer<-name+"viens de se connecter"
+					conn.Write([]byte("TCCHAT_USERIN\tEt bien, tu en as mis du temps pour decuver, vieux loup de mer!!!\n"))
+					answer := "Un vieux loup de mer vient de se connecter : "+name
+					fmt.Println(answer)
+					buffer<- answer
 					break
 				}else{
-					conn.Write([]byte("Bienvenu à bord, marin d'eau douce!!!"))
+					conn.Write([]byte("TCCHAT_USERIN\tBienvenu à bord, marin d'eau douce!!!\n"))
 					ioutil.WriteFile("whiteList.txt",[]byte(name+";"),0644)
-					buffer<-"Bienvenu à "+name+", un nouveau moussaillon"
+					answer := "Bienvenu à "+name+", un nouveau moussaillon"
+					fmt.Println(answer)
+					buffer<- answer
 					break
 				}
 			}
@@ -140,46 +185,52 @@ func handleConnection (conn net.Conn, buffer chan string, tabConn [10]bool, inde
 		}
 	}
 
-	if(!blackListed){
+	if(!blackListed) {
 
-		exit :=false
+		fmt.Println("Je rentre")
+		exit := false
 
-		for exit==false {
+		myReader := bufio.NewReader(conn)
 
-			buf := make([]byte, 0, 4096) // big buffer
+		for exit == false {
 
-			_, err := conn.Read(buf)
-
+			message, err := myReader.ReadString('\n')
 			if err != nil {
 				if err != io.EOF {
 					fmt.Println("Read error:", err)
-				}
-				break
-			}
-
-			blabla:=string(buf)
-
-			fmt.Println(blabla)
-
-			if(blabla!=""){
-				switch blabla {
-				case "exit":
-					exit=true
+					fmt.Println("Pouet!")
 					break
-				default:
-					buffer<-"["+strings.ToUpper(name)+"]"+blabla
 				}
 			}
 
+			message = strings.TrimSuffix(message, "\n")
 
+			fmt.Println(message)
 
+			if (message != "") {
+
+			if strings.EqualFold(message,"exit") {
+
+					exit = true
+
+				}else {
+
+					/*in:="["
+					upper:= strings.ToUpper(name)
+					out :="] ->"
+
+					message+=in+upper+out*/
+
+					buffer <-message
+				}
+			}
 		}
 	}
 
-	conn.Write([]byte("Bon vent mon gas!!!"))
-	tabConn[index]=true
-	conn.Close();
 
+	conn.Write([]byte("Bon vent mon gas!!!"))
+	*pC=true
+	conn.Close()
 
 }
 
@@ -215,8 +266,6 @@ func readFile(filePath string) [] string{
 		fmt.Print(err)
 	}
 
-	fmt.Println(reader) // print the content as 'bytes'
-
 	str := string(reader)
 
 	data:= strings.Split(str,";")
@@ -224,9 +273,11 @@ func readFile(filePath string) [] string{
 	return data
 }
 
-func clock(){
-	for{
-
-		time.Sleep(500)
+func join(strs ...string) string {
+	var sb strings.Builder
+	for _, str := range strs {
+		sb.WriteString(str)
 	}
+	return sb.String()
 }
+
